ca795883b5588878fe16214351efcee6543d6776
Jon Gutierrez <gutijon48@gmail.com>
2025-11-04 11:24:34 +0100

    Login/Registro con css

---
+++ Reto_DIW/php/burscarEmpleado.php
@@ -0,0 +1,18 @@
+<?php
+include("conexion.php");
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM empleados WHERE usuario = '$usuario' AND password = '$password'";
+$result = mysqli_query($conexion, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // El usuario y la contraseña son correctos
+    session_start();
+    $_SESSION['usuario'] = $usuario;
+    header("Location: ../html/inicio.html");
+} else {
+    // El usuario o la contraseña son incorrectos
+    echo "No existe ese usuario";
+}
+?>
