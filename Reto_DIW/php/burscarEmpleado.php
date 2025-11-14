f36bdd0e6da9e12e17432907cb76a65e011b0fc0
Jon Gutierrez <gutijon48@gmail.com>
2025-11-14 08:27:13 +0100

    Resolviendo conflictos

---
+++ Reto_DIW/php/burscarEmpleado.php
@@ -0,0 +1,25 @@
+<?php
+include("conexion.php");
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+
+$password_bd = "SELECT password FROM trabajadores WHERE usuario = '$usuario'";
+$result = mysqli_query($conexion, $password_bd);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $hashed_password = trim($row['password']);
+
+    if (password_verify($password, $hashed_password)) {
+        session_start();
+        $_SESSION['usuario'] = $usuario;
+        header("Location: eleccion.php");
+    } else {
+        echo "Contraseña incorrecta";
+    }
+} else   {
+
+    echo "No existe ese usuario";
+}
+?>
